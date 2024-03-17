25ba051a9353b5139053a123e280fc776035917c
Jacob <1jatora@gmail.com>
2024-03-17 19:37:05 +0400

    Level 2

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express"
 
 const app = express()
+app.use(express.json())
 
 const port = 3000
 
@@ -17,6 +18,26 @@ app.get("/books/:id", (req: Request, res: Response) => {
   const { id } = req.params
   const foundBook = books[Number(id)]
   res.status(200).json(foundBook)
+})
+
+app.post("/books", (req: Request, res: Response) => {
+  const { title, author, published } = req.body
+  books.push({ title, author, published })
+  res.status(201).json(books)
+})
+
+app.put("/books/:id", (req: Request, res: Response) => {
+  const { title, author, published } = req.body
+  const { id } = req.params
+  const updatedBook = { title, author, published }
+  books[Number(id)] = updatedBook
+  res.status(200).json(books)
+})
+
+app.delete("/books/:id", (req: Request, res: Response) => {
+  const { id } = req.params
+  books.splice(Number(id), 1)
+  res.status(200).json(books)
 })
 
 app.listen(port, () => {
